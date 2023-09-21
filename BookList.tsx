52e5f8a135865100371fa2172d6e9779d16a9001
joshwavetti <64787098+joshwavetti@users.noreply.github.com>
2023-09-21 17:43:46 +0200

    Add files via upload

---
+++ BookList.tsx
@@ -0,0 +1,56 @@
+import { IBook } from "./Book.type";
+import "./BookList.style.css";
+import BookModal from "./BookModal";
+import { useState } from "react";
+
+type Props = {
+  list: IBook[];
+  onDeleteClickHnd: (data: IBook) => void;
+  onEdit: (data: IBook) => void;
+};
+const BookList = (props: Props) => {
+  const { list, onDeleteClickHnd, onEdit } = props;
+  const [showModal, setShowModal] = useState(false);
+ const [dataToShow, setDataToShow] = useState(null as IBook | null);
+
+  const viewBook = (data: IBook) => {
+    setDataToShow(data);
+    setShowModal(true);
+  }
+
+ const onCloseModal = () => setShowModal(false);
+  return (
+    <div>
+      <article>
+        <h3 className="list-header">Book List</h3>
+      </article>
+      <table>
+        <tr>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Status</th>
+          <th>Actions</th>
+        </tr>
+        {list.map((book) => {
+          return (
+            <tr key={book.id}>
+              <td>{book.title}</td>
+              <td>{book.genre}</td>
+              <td>{book.status}</td>
+              <td>
+                <div>
+                  <input type="button" value="View" onClick={() => viewBook(book)}/>
+                  <input type="button" value="Edit" onClick={() => onEdit(book)} />
+                  <input type="button" value="Delete" onClick={() => onDeleteClickHnd(book)}/>
+                </div>
+              </td>
+            </tr>
+          );
+        })}
+      </table>
+      {showModal && dataToShow !== null && <BookModal onClose={onCloseModal} data={dataToShow} />}
+    </div>
+  );
+};
+
+export default BookList;
---
+++ Home.tsx
@@ -0,0 +1,82 @@
+import "./Home.style.css";
+import { useState } from "react";
+import { IBook, PageEnum, dummyBookList } from "./Book.type";
+import BookList from "./BookList";
+import AddBook from "./AddBook";
+import EditBook from "./EditBook";
+
+const Home = () => {
+  const [bookList, setBookList] = useState(dummyBookList as IBook[]);
+
+  const [shownPage, setShownPage] = useState(PageEnum.list);
+  const [dataToEdit, setDataToEdit] = useState({} as IBook);
+  const onAddBookClickHnd = () => {
+    setShownPage(PageEnum.add);
+  };
+
+  const showListPage = () => {
+    setShownPage(PageEnum.list);
+  };
+
+  const addBook = (data: IBook) => {
+    setBookList([...bookList, data]);
+  };
+  const deleteBook = (data: IBook) => {
+
+    const indexToDelete = bookList.indexOf(data);
+    const tempList = [...bookList]
+
+    tempList.splice(indexToDelete, 1);
+    setBookList(tempList);
+
+  };
+
+  const editBookData = (data: IBook) => {
+    setShownPage(PageEnum.edit);
+    setDataToEdit(data);
+
+  };
+
+const updateData = (data: IBook) => {
+const filteredData = bookList.filter(x => x.id === data.id)[0];
+const indexOfRecord = bookList.indexOf(filteredData);
+const tempData = [...bookList];
+tempData[indexOfRecord] = data;
+setBookList(tempData)
+}
+
+  return ( 
+    <>
+      <article className="article-header">
+        <header>
+          <h1> Book Catalogue</h1>
+        </header>
+      </article>
+
+      <section className="section-content">
+        {shownPage === PageEnum.list && (
+          <>
+            <input
+              type="button"
+              value="Add Book"
+              onClick={onAddBookClickHnd}
+              className="add-book-btn"
+            />
+            <BookList list={bookList} onDeleteClickHnd={deleteBook} onEdit={editBookData}/>
+          </>
+        )}
+
+        {shownPage === PageEnum.add && (
+          <AddBook
+            onBackBtnClickHnd={showListPage}
+            onSubmitClickHnd={addBook}
+          />
+        )}
+
+        {shownPage === PageEnum.edit && <EditBook data={dataToEdit} onBackBtnClickHnd={showListPage} onUpdateClickHnd={updateData}/>}
+      </section>
+    </>
+  );
+};
+
+export default Home;
